5968b2d4cd98b661160900be90de3ca7bf8917d8
destroyer <823134664@qq.com>
2019-05-27 16:38:43 +0800

    Signed-off-by: destroyer <823134664@qq.com>

--- app/src/main/java/android/bignerdranch/crimeactivity/CrimeActivity.java
+++ app/src/main/java/android/bignerdranch/crimeactivity/CrimeActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.UUID;
 
 public abstract class CrimeActivity extends SingleFragmentActivity {
 
@@ -29,6 +33,16 @@ public abstract class CrimeActivity extends SingleFragmentActivity {
         }
     }
     */
+
+    public static final String EXTRA_CRIME_ID =
+            "android.bignerdranch.crimeactivity.crime_id";
+
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment(){
--- app/src/main/java/android/bignerdranch/crimeactivity/CrimeListFragment.java
+++ app/src/main/java/android/bignerdranch/crimeactivity/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package android.bignerdranch.crimeactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -68,8 +69,12 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),
-            mCrime.getTitle() + " clicked!",Toast.LENGTH_SHORT).show();
+           /* Toast.makeText(getActivity(),
+            mCrime.getTitle() + " clicked!",Toast.LENGTH_SHORT).show();*/
+           //使用启动Activity来替代toast消息处理代码
+            //Intent intent = new Intent(getActivity(),CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            startActivity(intent);
         }
     }
 
